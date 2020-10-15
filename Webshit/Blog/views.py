0f4b41d05fdfb8954d64842c5383f1a2d3d7596a
Rabih <rabihutomo11@gmail.com>
2020-10-15 15:50:30 +0700

    restore my previous data

--- Webshit/Blog/views.py
+++ Webshit/Blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def Homepage(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'Blog/home.html', context)
+
+def about(request):
+    return render(request, 'Blog/about.html', {'title': 'About'})

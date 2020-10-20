c4dd69a74af91a9d3e2b63b2f3d899af0fca311e
Rabih <rabihutomo11@gmail.com>
2020-10-20 09:17:18 +0700

    adding profile picture and many other feature

--- Webshit/Blog/urls.py
+++ Webshit/Blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='Blog-Homepage'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),   # pk = primary key
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'), 
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'), 
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='Blog-About'),
 ]
--- Webshit/users/signals.py
+++ Webshit/users/signals.py
@@ -1,4 +1,4 @@
-'''from django.db.models.signals import post_save
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
@@ -10,6 +10,6 @@ def create_profile(sender, instance, created, **kwargs):
         Profile.objects.create(user=instance)
 
 
-@receiver(post_save, sender=User)
+'''@receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()'''
